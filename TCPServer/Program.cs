7dc23cd78df31e3a09ca50f2ffb8e4d0b02aa209
NeAlex9 <68583405+NeAlex9@users.noreply.github.com>
2021-05-01 15:15:04 +0300

    02.Work var

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TCPServer
 {
     public class TCPServer
     {
-        public void SendData(NetworkStream stream, string message)
+        public void SendData(NetworkStream stream, StringBuilder message)
         {
-            byte[] data = Encoding.ASCII.GetBytes(message);
+            byte[] data = Encoding.ASCII.GetBytes(message.ToString());
             stream.Write(data, 0, data.Length);
         }
 
@@ -27,6 +29,48 @@ namespace TCPServer
             }
 
             return receive.ToString();
+        }
+
+        private StringBuilder FindInformation(string source)
+        {
+            StringBuilder inf = new StringBuilder() { };
+            using (StreamReader reader = new StreamReader("domens.txt"))
+            {
+                string line = line = reader.ReadLine().Replace(@"\r\n", ""); ;
+                bool isFindDomen = false;
+                while (line != null && !isFindDomen)
+                {
+                    Regex reg = new Regex(@"\r\n");
+                    string sourceWithoutSpecSymb = source.TrimEnd('\r', '\n');
+                    if (line == sourceWithoutSpecSymb && reg.IsMatch(source))
+                    {
+                        line = reader.ReadLine();
+                        reg = new Regex(@".\\r\\n$");
+                        while (line != null && !reg.IsMatch(line))
+                        {
+                            inf.Append(line + "\n");
+                            line = reader.ReadLine();
+                        }
+
+                        isFindDomen = true;
+                    }
+                    line = reader.ReadLine();
+                    line = (line != null) ? line.Replace(@"\r\n", "") : line;
+
+                };
+
+                if (!isFindDomen)
+                {
+                    inf.Append("%This query returned 0 objects.\n\n");
+                    inf.Append($"%You queried for {source.TrimEnd('\r', '\n')} but this server does not have any data for {source.TrimEnd('\r', '\n')}\n\n");
+                }
+                else
+                {
+                    inf.Insert(0, "%This query returned 1 objects.\n\n");
+                }
+
+            }
+            return inf;
         }
 
         public void tcp()
@@ -48,9 +92,9 @@ namespace TCPServer
                     string query = GetData(stream, client);
                     Console.WriteLine("Client: " + query);
 
-                    string response = "I'am get your data, domen is world";
-                    SendData(stream, response);
-                    Console.WriteLine("*Server: " + response);
+                    StringBuilder inf = FindInformation(query);
+                    SendData(stream, inf);
+                    /*Console.WriteLine("*Server: " + response);*/
 
                     stream.Close();
                     client.Close();
@@ -75,6 +119,7 @@ namespace TCPServer
         {
             TCPServer tcp = new TCPServer();
             tcp.tcp();
+            Console.ReadLine();
         }
     }
 }
